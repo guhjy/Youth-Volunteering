3a747dd2b327218618906e58af718ccc019fd13a
Meenakshi Parameshwaran <meena.param@gmail.com>
2015-11-09 13:00:15 +0000

    Updating the OR plot to include social class and gender predictors.

--- volunteering_OR_plot.R
+++ volunteering_OR_plot.R
@@ -1,10 +1,8 @@
 # Title: Preparing an OR plot for volunteering frequency
 # Author: Meenakshi Parameshwaran
-# Date: 06/11/15
+# Date: 09/11/15
 
-# This script uses OR coefficient and standard error estimates from a random effects Stata model on volunteering frequency (taking from social_capital_analysis.do)
-
-# Full model output below
+# This script uses OR coefficient and standard error estimates from a random effects Stata model on volunteering frequency (taking from social_capital_analysis.do). Full model output:
 
 
 # Random-effects ordered logistic regression      Number of obs      =      3842
@@ -16,59 +14,50 @@
 # 
 # Integration method: mvaghermite                 Integration points =        12
 # 
-# Wald chi2(8)       =    257.79
-# Log likelihood  = -5493.8484                    Prob > chi2        =    0.0000
+# Wald chi2(10)      =    280.29
+# Log likelihood  = -5480.9078                    Prob > chi2        =    0.0000
 # 
 # ---------------------------------------------------------------------------------
 #     ypvolfreq | Odds Ratio   Std. Err.      z    P>|z|     [95% Conf. Interval]
 # ----------------+----------------------------------------------------------------
-#     ypdvage |   1.047174   .0247149     1.95   0.051     .9998374    1.096753
-# rural |   1.101814   .0928509     1.15   0.250     .9340641     1.29969
-# ypbothpar |   1.019053   .0762776     0.25   0.801     .8800013    1.180078
-# ypsrhlth |   1.077129   .0474582     1.69   0.092     .9880159     1.17428
-# rosenberg |   .9879482   .0111333    -1.08   0.282     .9663666    1.010012
-# friends20 |     1.0222   .0080822     2.78   0.005     1.006481    1.038164
-# religious_class |   2.109361   .2886818     5.45   0.000     1.613086    2.758317
-# cultural |   1.738193    .067235    14.29   0.000     1.611287    1.875096
+#     ypdvage |   1.044333   .0246073     1.84   0.066     .9972006    1.093694
+# rural |   1.119229   .0938004     1.34   0.179     .9496887    1.319035
+# ypbothpar |    1.01024    .091069     0.11   0.910     .8466272    1.205471
+# ypsrhlth |   1.084368   .0477002     1.84   0.066     .9947941    1.182007
+# rosenberg |   .9934524   .0112049    -0.58   0.560     .9717323    1.015658
+# friends20 |   1.023149   .0080677     2.90   0.004     1.007459    1.039085
+# religious_class |   2.081954   .2846493     5.36   0.000     1.592549    2.721756
+# cultural |   1.713555   .0664281    13.89   0.000     1.588182    1.848826
+# ypclass |   .9898003   .0199543    -0.51   0.611     .9514532    1.029693
+# ypfemale |   1.460088   .1094943     5.05   0.000      1.26051    1.691267
 # ----------------+----------------------------------------------------------------
-#     /cut1 |   .5278799   .4269552     1.24   0.216    -.3089368    1.364697
-# /cut2 |   1.623673   .4283417     3.79   0.000     .7841386    2.463207
-# /cut3 |    2.63759   .4309388     6.12   0.000     1.792965    3.482214
-# /cut4 |   3.411709   .4338406     7.86   0.000     2.561397    4.262021
-# /cut5 |   4.758612   .4425138    10.75   0.000     3.891301    5.625923
+#     /cut1 |   .7980064    .450568     1.77   0.077    -.0850907    1.681103
+# /cut2 |   1.894024   .4522147     4.19   0.000     1.007699    2.780348
+# /cut3 |   2.909289   .4549838     6.39   0.000     2.017537    3.801041
+# /cut4 |   3.684885   .4579224     8.05   0.000     2.787373    4.582396
+# /cut5 |   5.033478   .4664061    10.79   0.000     4.119339    5.947617
 # ----------------+----------------------------------------------------------------
-#     /sigma2_u |   .7727427   .1281506                      .5583067     1.06954
+#     /sigma2_u |   .7414365   .1259004                      .5315385    1.034221
 # ---------------------------------------------------------------------------------
-#     LR test vs. ologit regression:   chibar2(01) =    57.48 Prob>=chibar2 = 0.0000
+#     LR test vs. ologit regression:   chibar2(01) =    54.32 Prob>=chibar2 = 0.0000
 
 
 # Prepare dataframe with values -------------------------------------------
 
 # this is just a copy paste from the model above, except I have renamed the variables
 
-var <- c("Age", "Rural", "Lives with both parents", "Self-reported health", "Rosenberg scale score", "Number of close friends", "Attends religious classes", "Cultural capital")
-or <- c(1.047174, 1.101814, 1.019053, 1.077129, .9879482, 1.0222, 2.109361, 1.738193)
-se <- c(.0247149, .0928509, .0762776, .0474582, .0111333, .0080822, .2886818, .067235)
-sig <- c("Insignificant", "Insignificant", "Insignificant", "Insignificant", "Insignificant", "Significant", "Significant", "Significant")
+var <- c("Age", "Rural", "Lives with both parents", "Self-reported health", "Rosenberg scale score", "Number of close friends", "Attends religious classes", "Cultural capital", "Social class", "Female")
+or <- c(1.044333, 1.119229, 1.01024, 1.084368, .9934524, 1.023149, 2.081954, 1.713555, .9898003, 1.460088)
+se <- c(.0246073, .0938004, .091069, .0477002, .0112049, .0080677, .2846493, .0664281, .0199543, .0199543)
+sig <- c("Insignificant", "Insignificant", "Insignificant", "Insignificant", "Insignificant", "Significant", "Significant", "Significant", "Insignificant", "Significant")
 ub <- or + (1.96*se)
 lb <- or - (1.96*se)
 
 # combine these into a dataframe
-mydf <- data.frame(cbind(var, or, se, sig, ub, lb))
+mydf <- data.frame(var, or, se, sig, ub, lb)
 
 # check the datatypes
-str(mydf) # ok everything is a factor, when most should be numeric
-
-# convert numeric variables back to numbers - here's a short function to do this
-as.numeric.factor <- function(x) {as.numeric(levels(x))[x]}
-
-# call the function on the target vars
-mydf <- data.frame(mydf[c(1,4)], lapply(mydf[c(2,3,5,6)], as.numeric.factor))
-
-# check the data types have changed
-str(mydf)
-
-# or could do - lapply(mydf, function(x) class(x))
+str(mydf) # ok everything in the correct data type
 
 # order by size of effect
 mydf <- mydf[order(-or),] # minus sign in front of OR orders mydf in decreasing order
@@ -82,7 +71,7 @@ ggplot(mydf, aes(x = var, y = or)) +
     geom_hline(yintercept = 1.0, colour = gray(1/2), lty = 2) + 
 #     theme_classic() + 
     theme(legend.position="bottom", legend.title = element_blank()) +
-    scale_x_discrete("Youth characteristics", limits = c("Rosenberg scale score","Lives with both parents", "Number of close friends", "Age", "Self-reported health", "Rural", "Cultural capital", "Attends religious classes")) + 
+    scale_x_discrete("Youth characteristics", limits = c("Social class", "Rosenberg scale score","Lives with both parents", "Number of close friends", "Age", "Self-reported health", "Rural", "Female", "Cultural capital", "Attends religious classes")) + 
     scale_y_continuous("Odds-ratio", limits = c(0.8, 2.8), breaks = c(0.8, 1, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8)) + 
     coord_flip() + 
     guides(size = FALSE)
